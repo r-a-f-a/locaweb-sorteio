92c50fc137a741dd8b0a2664567e6d90aedde009
Wesley <wferreira@allin.com.br>
2019-11-19 17:19:45 -0300

    [COMPONENT] new components in describe [image][text]

--- src/components/themes/series/contest/components/describe/index.vue
+++ src/components/themes/series/contest/components/describe/index.vue
@@ -2,30 +2,36 @@
   <div class="describe">
     <div class="row">
       <div class="col-md-6">
-        <h1>Sorteio categoria geral</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores, quas ullam maiores atque optio vitae magnam praesentium quaerat quae alias, non distinctio doloribus consequuntur recusandae. Nisi eius totam maiores illo?</p>
+        <textTitle :configs="configs"></textTitle>
       </div>
       <div class="col-md-6">
-        <img class="image-describe" src="https://st3.depositphotos.com/2899123/18626/v/1600/depositphotos_186264814-stock-illustration-realistic-silver-champion-award-medal.jpg" alt="Pratas">
+        <cardImage :configs="configs"></cardImage>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import textTitle from './components/text'
+import cardImage from './components/image'
+
 export default {
   name: 'Describe',
   props: ['configs'],
+  components: {
+    textTitle,
+    cardImage
+  },
   data () {
     return {
       data: ''
     }
-  },
-  created () {
   }
 }
 </script>
 
 <style>
-
+.margin{
+  margin: 20px;
+}
 </style>
